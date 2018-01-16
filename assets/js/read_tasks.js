ca19803b550b212bdfaa65ea7cd6813513a3f74a
danijelkelava <dykyprod@gmail.com>
2018-01-16 17:14:10 +0100

    getjson

--- assets/js/read_tasks.js
+++ assets/js/read_tasks.js
@@ -10,8 +10,29 @@ $(document).ready(function(){
 			read_tasks="";
 
 			$.each(data, function(key, val){
-				read_tasks += "<p>" + val.id + "</p>";
-				read_tasks += "<p>"+ val.naziv_taska + "</p>";
+
+					read_tasks += "<table class='table'>";
+					read_tasks += "<thead>";
+					    read_tasks += "<tr>";
+					      read_tasks += "<th scope='col'>Ime taska</th>";
+					      read_tasks += "<th scope='col'>Prioritet</th>";
+					      read_tasks += "<th scope='col'>Rok</th>";
+					      read_tasks += "<th scope='col'>Status</th>";
+					      read_tasks += "<th scope='col'></th>";
+					      read_tasks += "<th scope='col'></th>";
+					      read_tasks += "<th scope='col'></th>";
+					      read_tasks += "<th scope='col'></th>";
+					    read_tasks += "<tr>";
+					  read_tasks += "<thead>";
+					  read_tasks += "<tbody>";
+					    read_tasks += "<tr>";
+					      read_tasks += "<td>" + val.naziv_taska + "</td>";
+					      read_tasks += "<td>" + val.prioritet + "</td>";
+					      read_tasks += "<td>" + val.rok + "</td>";
+					      read_tasks += "<td>" + val.status + "</td>";
+					    read_tasks += "</tr>";
+					  read_tasks += "</tbody>";
+					read_tasks += "</table>";
 			});
 			$('#test').html(read_tasks);
 		});
